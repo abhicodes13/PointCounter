ff142a6bf0c49e7ca04fe7ee1321815e661fb5ae
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2022-04-28 20:04:20 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,3 +1,6 @@
+// Making a function with the name points that has two arguements. 
+
+
 function points(2pointer,3pointer){
-return (2pointer * 2 + 3pointer * 3);
+  return (2pointer * 2 + 3pointer * 3);
 }
